049d003f1ac6044c25e003ea507981db56831342
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2024-02-10 23:38:50 -0500

    Refactored WikiArticleInputName

--- src/main/java/edu/bsu/cs/wikipedia/WikiArticleInputName.java
+++ src/main/java/edu/bsu/cs/wikipedia/WikiArticleInputName.java
@@ -1,19 +1,22 @@
 package edu.bsu.cs.wikipedia;
+
 import java.util.Scanner;
 
 public class WikiArticleInputName {
-    public String articleName(){
-        Scanner scanner = new Scanner(System.in);
+
+    private String selectedArticle;
+
+    public String articleName(Scanner scanner) {
         System.out.println("Enter a Wiki Article Name:");
-        return scanner.nextLine();
+        this.selectedArticle = scanner.nextLine();
+        return this.selectedArticle;
     }
 
-
-
+    public String getSelectedArticle() {
+        return this.selectedArticle;
     }
 
-
-
+}
 
 
 
