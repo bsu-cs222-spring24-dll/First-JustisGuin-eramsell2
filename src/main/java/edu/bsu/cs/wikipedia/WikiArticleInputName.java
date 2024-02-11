45f8c61428e78aa3ed287e8478eef38cbd2533eb
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2024-02-10 22:18:20 -0500

    Refactor WikiArticleInputName

--- src/main/java/edu/bsu/cs/wikipedia/WikiArticleInputName.java
+++ src/main/java/edu/bsu/cs/wikipedia/WikiArticleInputName.java
@@ -1,10 +1,5 @@
 package edu.bsu.cs.wikipedia;
-
-import java.util.Objects;
 import java.util.Scanner;
-
-import com.jayway.jsonpath.JsonPath;
-import net.minidev.json.JSONArray;
 
 public class WikiArticleInputName {
     public String articleName(){
@@ -14,22 +9,6 @@ public class WikiArticleInputName {
     }
 
 
-    public void isarticleNameEmpty(String jsonRevisionData){
-        if (Objects.equals(articleName(), "")) {
-            System.err.println("This Article is Empty");
-            System.exit(0);
-        }
-    }
-
-
-    public void checkArticleIsValid(String jsonRevisionData){
-        JSONArray articleRevisions = JsonPath.read(jsonRevisionData, "$..revisions");
-        if (articleRevisions.isEmpty()) {
-            System.err.println("This Article is Empty");
-            System.exit(0);
-        }
-
-
 
     }
 
@@ -39,4 +18,4 @@ public class WikiArticleInputName {
 
 
 
-}
+
