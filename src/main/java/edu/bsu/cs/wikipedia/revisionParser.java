bb8187f817a5f06e1c0e986144874af6280d1e86
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2024-02-11 14:58:17 -0500

    Refactor revisionParser

--- src/main/java/edu/bsu/cs/wikipedia/revisionParser.java
+++ src/main/java/edu/bsu/cs/wikipedia/revisionParser.java
@@ -5,21 +5,26 @@ import net.minidev.json.JSONArray;
 
 import java.io.IOException;
 import java.io.InputStream;
-public class revisionParser {
-    public JSONArray parse(InputStream testInputStream) throws IOException {
-        JSONArray names = (JSONArray) JsonPath.read(testInputStream, "$..revisions..user");
+import java.util.List;
 
+public class revisionParser {
+
+    public List<String> parse(InputStream testInputStream) throws IOException {
+        List<String> names = JsonPath.read(testInputStream, "$..revisions..user");
+        for (int i = 0; i < names.size(); i++) {
+            names.set(i, names.get(i));
+        }
         return names;
 
     }
     public JSONArray parseTimestamps(InputStream testInputStream) throws IOException{
-        JSONArray timestamps = (JSONArray) JsonPath.read(testInputStream, "$..revisions..timestamp");
-        return timestamps;
+        return (JSONArray) JsonPath.read(testInputStream, "$..revisions..timestamp");
     }
     public JSONArray parseRedirects(InputStream testInputStream) throws IOException{
-        JSONArray redirects = (JSONArray) JsonPath.read(testInputStream, "$..redirects..to");
-        return redirects;
+        return (JSONArray) JsonPath.read(testInputStream, "$..redirects..to");
     }
+
+
 
 
 
