99882895eaaf12c9cb6b9fef3ea1935100587737
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2024-02-13 09:13:26 -0500

    Refactored connectToWiki and revisionParser

--- src/main/java/edu/bsu/cs/wikipedia/connectToWiki.java
+++ src/main/java/edu/bsu/cs/wikipedia/connectToWiki.java
@@ -1,7 +1,5 @@
 package edu.bsu.cs.wikipedia;
 
-import com.jayway.jsonpath.JsonPath;
-import net.minidev.json.JSONArray;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,7 +8,7 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.net.UnknownHostException;
 import java.nio.charset.Charset;
-import java.rmi.RemoteException;
+
 import java.util.Scanner;
 
 public class connectToWiki {
--- src/main/java/edu/bsu/cs/wikipedia/revisionParser.java
+++ src/main/java/edu/bsu/cs/wikipedia/revisionParser.java
@@ -2,14 +2,13 @@ package edu.bsu.cs.wikipedia;
 
 import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
-
 import java.io.IOException;
 
 public class revisionParser {
 
     public String parse(String jsonData) throws IOException {
         JSONArray names = (JSONArray) JsonPath.read(jsonData, "$..revisions..user");
-        System.out.println("14 Most Recent Revisions:");
+        System.out.println(" User Revisions:");
         for (int i = 0; i < 14; i++) {
             System.out.println((i + 1) + ". " + names.get(i).toString());
         }
@@ -28,10 +27,10 @@ public class revisionParser {
     public String parseRedirects(String jsonData) throws IOException {
         JSONArray redirects = (JSONArray) JsonPath.read(jsonData, "$..redirects");
         if (!redirects.isEmpty()) {
-            JSONArray toArray = (JSONArray) JsonPath.read(jsonData, "$..redirects[0].to");
+            JSONArray toArray = (JSONArray) JsonPath.read(jsonData, "$..redirects.to");
             String redirectTo = "";
             if (!toArray.isEmpty()) {
-                redirectTo = (String) toArray.get(0);
+                redirectTo = (String) toArray.getFirst();
             }
             System.out.println("Redirected to: " + redirectTo);
         } else {
