b6b501ea31d853bc22ac1460725d128561e461b0
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2024-02-15 15:34:25 -0500

    Removed Comments in WikiArticleInputName

--- src/main/java/edu/bsu/cs/wikipedia/WikiArticleInputName.java
+++ src/main/java/edu/bsu/cs/wikipedia/WikiArticleInputName.java
@@ -8,8 +8,6 @@ import java.util.Scanner;
 
 public class WikiArticleInputName {
 
-    //Parse structured data using an appropriate and robust technology such as JsonPath: don't rely
-//on naive String searches to extract what data you need.
     public String articleName(Scanner scanner) {
         System.out.println("Enter a Wiki Article Name:");
 
